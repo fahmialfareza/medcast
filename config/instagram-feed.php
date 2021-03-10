9551a37ceb283635238c7e1380083482ea9b11ca
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-03-10 17:19:09 +0700

    Update some config

--- config/instagram-feed.php
+++ config/instagram-feed.php
@@ -4,13 +4,13 @@ return [
     /*
      * The client_id from registering your app on Instagram
      */
-    'client_id'           => env('INSTAGRAM_CLIENT_ID'),
+    'client_id'           => '3187831094652817',
 
     /*
      * The client secret from registering your app on Instagram,
      * This is not the same as an access token.
      */
-    'client_secret'       => env('INSTAGRAM_CLIENT_SECRET'),
+    'client_secret'       => '46272c42c2cee2906b03ed80c14e2987',
 
     /*
      * The route that will respond to the Instagram callback during the OAuth process.
